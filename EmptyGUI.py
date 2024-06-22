0956cf637695bf171bffa776b32b7a7e2187f89b
mani15204 <manikandan.rajkanna04@gmail.com>
2024-06-22 19:43:22 +0530

    Commit: Test Commit EmptyGUI

--- EmptyGUI.py
+++ EmptyGUI.py
@@ -3,6 +3,20 @@ import tkinter as tk
 from tkinter import *
 from tkinter.constants import *
 
+'''
+
+    Creating a GUI using Tkinter in python is very easy.
+    It just takes two lines of code to build an empty GUI window.
+
+    root = Tk()
+    root.mainloop()
+
+    But let's explore little more from this
+
+'''
+
+
 # Building an empty GUI
 root = Tk()
+root.title("GUI title")
 root.mainloop()
